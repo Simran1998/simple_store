6568d26a7549131eedc32e66830fb8f5ddbe5e39
Simran Kaur <simran143663@gmail.com>
2023-02-26 16:28:45 -0600

    show action/view added and made accessible through index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "/products", to: "products#index"
+  # get "/products", to: "products#index"
+  # get "/products/:id", to: "products#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # resources :products
+  resources :products
 end
