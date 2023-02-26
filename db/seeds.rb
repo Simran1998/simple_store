a4fa6936ec05f694bbf9ae1a0843faf61eb14fe5
Simran Kaur <simran143663@gmail.com>
2023-02-26 17:05:43 -0600

    Created products.csv and changed seeds.db so that it now pulls data from the csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 
 Product.delete_all
+Category.destroy_all
 
-676.times do
-  Product.create(
-    title:          Faker::Commerce.brand,
-    price:          Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 1, to: 1000)
-  )
+# 676.times do
+#   Product.create(
+#     title:          Faker::Commerce.brand,
+#     price:          Faker::Commerce.price,
+#     stock_quantity: Faker::Number.between(from: 1, to: 1000)
+#   )
+# end
+#
+# puts "Created #{Product.count} Products"
+
+filename = Rails.root.join("db/products.csv")
+puts "Loading products from the csv file: #{filename}"
+
+csv_data = File.read(filename)
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p["category"])
+  if category && category.valid?
+    product = category.products.create(
+      title:          p["name"],
+      price:          p["price"],
+      description:    p["description"],
+      stock_quantity: p["stock quantity"]
+    )
+    puts "Invalid product #{p['name']}" unless product&.valid?
+  else
+    puts "invalid category #{p['category']} for product #{p['name']}."
+  end
+
+  # puts m['original_title']
 end
-
-puts "Created #{Product.count} Products"
+puts "Created #{Category.count} categories"
+puts "Created #{Product.count} products."
